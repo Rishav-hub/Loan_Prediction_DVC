c46e99ebdf411ea01c35c0cbe6c3de693630707a
Rishav-hub <vibhanshuchauhan200@gmail.com>
2021-10-15 17:04:51 +0530

    added Xgboost model

--- src/stage_03_train.py
+++ src/stage_03_train.py
@@ -4,7 +4,7 @@ import argparse
 import pandas as pd
 import os
 from sklearn.ensemble import RandomForestClassifier
-# from xgboost import XGBClassifier
+from xgboost import XGBClassifier
 import pickle
 import logging
 
@@ -48,6 +48,8 @@ def training_data(config_path, params_path):
     max_features = params['model_params']['random_forest']['max_features']
     min_samples_split = params['model_params']['random_forest']['min_samples_split']
     min_samples_leaf = params['model_params']['random_forest']['min_samples_leaf']
+    model_name = params['model_params']['random_forest']['model_name']
+
     random_state= params['base']['random_state']
 
     # Create the model
@@ -62,7 +64,7 @@ def training_data(config_path, params_path):
 
     # Save the model
     model_dir = config["artifacts"]["model_dir"]
-    model_filename = config["artifacts"]["model_file"]
+    model_filename = config["artifacts"]["model_file_random_forest"]
 
     model_dir = os.path.join(artifacts_dir, model_dir)
 
@@ -72,7 +74,46 @@ def training_data(config_path, params_path):
 
     pickle.dump(model, open(model_path, 'wb'))
 
+    logging.info(f"Training completed for {model_name} !!!!!")
 
+
+    # Load Parameters for Xgboost
+    model_name = params['model_params']['xgboost']['model_name']
+    n_estimators = params['model_params']['xgboost']['n_estimators']
+    max_depth = params['model_params']['xgboost']['max_depth']
+    learning_rate = params['model_params']['xgboost']['learning_rate']
+    min_child_weight = params['model_params']['xgboost']['min_child_weight']
+    gamma = params['model_params']['xgboost']['gamma']
+    subsample = params['model_params']['xgboost']['subsample']
+    colsample_bytree = params['model_params']['xgboost']['colsample_bytree']
+    max_delta_step = params['model_params']['xgboost']['max_delta_step']
+
+    # Create the model
+    model = XGBClassifier(n_estimators= n_estimators,
+                          max_depth= max_depth,
+                          learning_rate= learning_rate,
+                          min_child_weight= min_child_weight,
+                          gamma= gamma,
+                          subsample= subsample,
+                          colsample_bytree= colsample_bytree,
+                          max_delta_step= max_delta_step)
+    
+    model.fit(train_X, train_y)
+
+    # Save the model
+    model_dir = config["artifacts"]["model_dir"]
+    model_filename = config["artifacts"]["model_file_xgboost"]
+
+    model_dir = os.path.join(artifacts_dir, model_dir)
+
+    create_directory([model_dir])
+
+    model_path = os.path.join(model_dir, model_filename)
+
+    pickle.dump(model, open(model_path, 'wb'))
+
+    logging.info(f"Training completed for {model_name} !!!!!")
+  
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', '-c',type=str, default='config/config.yaml')
